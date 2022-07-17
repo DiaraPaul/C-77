caeb9e08c7cc45d7b86a3c278465112ce5bb4870
DiaraPaul <88132918+DiaraPaul@users.noreply.github.com>
2022-07-17 18:39:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name= "Diara"
+console.log(first_name);
+
+var last_name= "Paul"
+console.log(last_name);
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+var a = 7;
+var b = 5;
+var c = a+b ;
+console.log(c);
